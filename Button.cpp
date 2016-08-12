4d94f30526dbff927198cc110db0ac1085ec515b
Isakovp <isakovp@gmail.com>
2016-08-12 19:31:47 +0300

    first commit

---
+++ Button.cpp
@@ -0,0 +1,59 @@
+
+#include "Button.h"
+
+Button::Button(int btnPin) {
+  pin = btnPin;
+  prevButtonState = LOW;
+  lastDebounceTime = 0;
+}
+
+bool Button::scan(void) {
+  if (p_handler != NULL || c_handler != NULL) {
+    int currentState = digitalRead(pin);
+
+    if (currentState != prevButtonState) {
+      lastDebounceTime =  millis();
+    }
+
+    if ((millis() - lastDebounceTime) > DEBOUNCE_TIME) {
+
+     if (currentState != buttonState) {
+        buttonState = currentState;
+
+        if (buttonState == HIGH) {
+          pressTime = millis();
+          if (p_handler != NULL) {
+            p_handler(*this);
+          }
+        }
+
+
+        if (buttonState == LOW) {
+          if (millis() - pressTime > LONG_CLICK_TIME) {
+            pressTime = millis();
+            if (cl_handler != NULL) {
+              cl_handler(*this);
+            }
+          } else {
+            if (c_handler != NULL) {
+              c_handler(*this);
+            }
+          }
+        }
+      }
+    }
+    prevButtonState = currentState;
+  }
+}
+
+void Button::setPressHandler(buttonEventHandler h) {
+  p_handler = h;
+}
+
+void Button::setClickHandler(buttonEventHandler h) {
+  c_handler = h;
+}
+
+void Button::setLongClickHandler(buttonEventHandler h) {
+  cl_handler = h;
+}
---
+++ Button.h
@@ -0,0 +1,36 @@
+#if defined(ARDUINO)
+#include "Arduino.h"
+#endif
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#define DEBOUNCE_TIME 50
+#define LONG_CLICK_TIME 300
+
+class Button;
+typedef void (*buttonEventHandler)(Button&);
+
+class Button {
+
+  public:
+    Button(int btnPin);
+
+    bool scan();
+    void setPressHandler(buttonEventHandler handler);
+    void setClickHandler(buttonEventHandler handler);
+    void setLongClickHandler(buttonEventHandler handler);
+  private:
+    int pin;
+    int prevButtonState;
+    int buttonState;
+    long lastDebounceTime;
+    long pressTime;
+    buttonEventHandler c_handler;
+    buttonEventHandler p_handler;
+    buttonEventHandler cl_handler;
+
+
+};
+#endif
+
